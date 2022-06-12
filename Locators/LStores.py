876f0a174ab3aefe010c548d306f09f4b55707a7
netzanet tadesse <netzanet123@gmail.com>
2022-06-12 10:32:06 +0300

    stores page

---
+++ Locators/LStores.py
@@ -0,0 +1,26 @@
+
+class LocatorsStores:
+    ##login locators
+    phoneField = "//body/div[@id='root']/div[1]/div[2]/div[1]/span[1]/form[1]/div[1]/div[1]/span[1]/div[1]/input[1]"
+    phone = "1950000000"
+    clickSend = "//body/div[@id='root']/div[1]/div[2]/div[1]/span[1]/form[1]/input[1]"
+    passwordField = "//body/div[@id='root']/div[1]/div[2]/div[1]/span[1]/form[1]/div[1]/div[1]/span[1]/div[1]/input[1]"
+    password = "1234"
+    clickLog = "//body/div[@id='root']/div[1]/div[2]/div[1]/span[1]/form[1]/input[1]"
+    #add/update store locators
+    addButton= "//body/div[@id='root']/div[1]/div[2]/main[1]/div[2]/div[1]/div[1]/div[1]/span[1]/i[1]"
+    adding = "//body/div[@id='root']/div[1]/div[2]/main[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]"
+    bnNumField= "//body/div[@id='root']/div[1]/div[4]/div[1]/div[1]/form[1]/div[1]/div[1]/span[1]/div[1]/input[1]"
+    nameField= "//body/div[@id='root']/div[1]/div[4]/div[1]/div[1]/form[1]/div[1]/div[2]/span[1]/div[1]/input[1]"
+    websiteField= "//body/div[@id='root']/div[1]/div[4]/div[1]/div[1]/form[1]/div[1]/div[3]/span[1]/div[1]/input[1]"
+    descriptionField= "//body/div[@id='root']/div[1]/div[4]/div[1]/div[1]/form[1]/div[1]/div[4]/span[1]/div[1]/input[1]"
+    telephoneField= "//body/div[@id='root']/div[1]/div[4]/div[1]/div[1]/form[1]/div[1]/div[7]/span[1]/div[1]/input[1]"
+    emailField="//body/div[@id='root']/div[1]/div[4]/div[1]/div[1]/form[1]/div[1]/div[8]/span[1]/div[1]/input[1]"
+    departmentField= "//body/div[@id='root']/div[1]/div[4]/div[1]/div[1]/form[1]/div[1]/div[9]/span[1]/div[3]/div[1]"
+    cityField ="//body/div[@id='root']/div[1]/div[4]/div[1]/div[1]/form[1]/div[1]/div[10]/div[1]/div[1]/span[1]/div[1]/input[1]"
+    streetField= "//body/div[@id='root']/div[1]/div[4]/div[1]/div[1]/form[1]/div[1]/div[10]/div[1]/div[2]/span[1]/div[1]/input[1]"
+    buildingField= "//body/div[@id='root']/div[1]/div[4]/div[1]/div[1]/form[1]/div[1]/div[10]/div[1]/div[3]/span[1]/div[1]/input[1]"
+    apartmentField= "//body/div[@id='root']/div[1]/div[4]/div[1]/div[1]/form[1]/div[1]/div[10]/div[1]/div[4]/span[1]/div[1]/input[1]"
+    addStoreButton= "//body/div[@id='root']/div[1]/div[4]/div[1]/div[1]/form[1]/input[1]"
+    ##search store locators
+    searchStorefield= "//body/div[@id='root']/div[1]/div[2]/main[1]/div[2]/div[1]/div[1]/span[1]/span[1]/div[1]/input[1]"
