f54825e0f94219f1a1dfdc6a474018982bd15607
miki <mikinagash1997@gmail.com>
2022-06-14 21:15:11 +0300

    commit number 8

---
+++ Tests/Server/API_Orders.py
@@ -0,0 +1,138 @@
+import requests
+
+
+class TestSearch:
+    url = "https://qa-api.trado.co.il/api/order/filter"
+
+    def test_search_first_name_correct(self):
+        body = {"search": "עדי"}
+        res = requests.post(self.url,json=body)
+        response = res.json()
+        assert res.elapsed.total_seconds() < 10
+        assert res.status_code == 200
+        print(res.status_code)
+        print(response)
+
+
+
+
+    def test_search_last_name_correct(self):
+        body = {"search": "אמזלג"}
+        res = requests.post(self.url,json=body)
+        response = res.json()
+        assert res.elapsed.total_seconds() < 10
+        assert res.status_code == 200
+        print(res.status_code)
+        print(response)
+
+
+
+    def test_search_order_correct_by_partly_first_name(self):
+        body = {"search": "עד"}
+        res = requests.post(self.url, json=body)
+        response = res.json()
+        assert res.status_code == 200
+        print(res.status_code)
+        print(response)
+
+
+
+    def test_search_order_incorrect_by_first_name(self):
+        body = {"search": "גגג"}
+        res = requests.post(self.url, data=body)
+        response = res.json()
+        assert response['count'] == 0
+        assert res.elapsed.total_seconds() < 10
+        assert res.status_code == 200
+        print(res.status_code)
+        print(response)
+
+
+
+
+    def test_search_order_incorrect_with_special_characters(self):
+        body = {"search": "!#$&*"}
+        res = requests.post(self.url, json=body)
+        response = res.json()
+        assert res.status_code == 200
+        print(res.status_code)
+        print(response)
+
+
+
+
+    def test_search_order_correct_with_order_numbers(self):
+        body = {"search": "418"}
+        res = requests.post(self.url, json=body)
+        response = res.json()
+        assert res.status_code == 200
+        print(res.status_code)
+        print(response)
+
+
+
+
+    def test_search_order_correct_with_invalid_order_numbers(self):
+        body = {"search": "1335"}
+        res = requests.post(self.url, json=body)
+        response = res.json()
+        assert res.status_code == 200
+        print(res.status_code)
+        print(response)
+
+
+
+
+
+    def test_search_order_correct_with_bankTransfer_payment(self):
+        body = {"search": "bankTransfer"}
+        res = requests.post(self.url, json=body)
+        response = res.json()
+        assert res.status_code == 200
+        print(res.status_code)
+        print(response)
+
+
+
+
+
+    def test_search_order_correct_with_b2b_payment(self):
+        body = {"search": "bankTransfer"}
+        res = requests.post(self.url, json=body)
+        response = res.json()
+        assert res.status_code == 200
+        print(res.status_code)
+        print(response)
+
+
+
+
+    def test_search_order_correct_with_etrado_payment(self):
+        body = {"search": "etrado"}
+        res = requests.post(self.url, json=body)
+        response = res.json()
+        assert res.status_code == 200
+        print(res.status_code)
+        print(response)
+
+
+
+    def test_search_order_correct_with_invalid_payment(self):
+        body = {"search": "לאומי"}
+        res = requests.post(self.url, json=body)
+        response = res.json()
+        assert res.status_code == 200
+        print(res.status_code)
+        print(response)
+
+
+
+
+
+
+
+
+
+
+
+
