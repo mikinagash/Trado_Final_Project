ff1dc4d5fe643be85dedf184d95bab98e39491d1
miki <mikinagash1997@gmail.com>
2022-06-16 23:17:27 +0300

    commit number 12

--- Tests/Server/API_Orders.py
+++ Tests/Server/API_Orders.py
@@ -4,19 +4,18 @@ import requests
 
 class TestSearch:
     url = "https://qa-api.trado.co.il/api/order/filter"
-
+    # Bug
     def test_search_first_name_correct(self):
         body = {"search": "עדי"}
         res = requests.post(self.url,json=body)
         response = res.json()
         assert res.elapsed.total_seconds() < 10
+        assert response['payload']['count'] == 0
         assert res.status_code == 200
-        print(res.status_code)
-        print(response)
 
 
 
-
+    # Bug
     def test_search_last_name_correct(self):
         body = {"search": "אמזלג"}
         res = requests.post(self.url,json=body)
@@ -27,14 +26,15 @@ class TestSearch:
         print(response)
 
 
-
+    # Bug
     def test_search_order_correct_by_partly_first_name(self):
         body = {"search": "עד"}
         res = requests.post(self.url, json=body)
         response = res.json()
+        assert response['payload']['count'] == 0
         assert res.status_code == 200
         print(res.status_code)
-        print(response)
+        print(response['payload']['count'])
 
 
 
@@ -42,11 +42,9 @@ class TestSearch:
         body = {"search": "גגג"}
         res = requests.post(self.url, data=body)
         response = res.json()
-        assert response['count'] == 0
+        assert response['payload']['count'] == 0
         assert res.elapsed.total_seconds() < 10
-        assert res.status_code == 200
-        print(res.status_code)
-        print(response)
+
 
 
 
@@ -55,21 +53,20 @@ class TestSearch:
         body = {"search": "!#$&*"}
         res = requests.post(self.url, json=body)
         response = res.json()
-        assert res.status_code == 200
-        print(res.status_code)
-        print(response)
+        assert response['payload']['count'] == 0
+        print(response['payload']['count'])
 
 
 
-
+    # Bug
     def test_search_order_correct_with_order_numbers(self):
         body = {"search": "418"}
         res = requests.post(self.url, json=body)
         response = res.json()
+        assert response['payload']['count'] == 0
         assert res.status_code == 200
         print(res.status_code)
         print(response)
-
 
 
 
@@ -77,18 +74,19 @@ class TestSearch:
         body = {"search": "1335"}
         res = requests.post(self.url, json=body)
         response = res.json()
-        assert res.status_code == 200
+        assert response['payload']['count'] == 0
         print(res.status_code)
         print(response)
 
 
 
 
-
+    #Bug
     def test_search_order_correct_with_bankTransfer_payment(self):
         body = {"search": "bankTransfer"}
         res = requests.post(self.url, json=body)
         response = res.json()
+        assert response['payload']['count'] == 0
         assert res.status_code == 200
         print(res.status_code)
         print(response)
@@ -96,18 +94,19 @@ class TestSearch:
 
 
 
-
+    # Bug
     def test_search_order_correct_with_b2b_payment(self):
         body = {"search": "bankTransfer"}
         res = requests.post(self.url, json=body)
         response = res.json()
+        assert response['payload']['count'] == 0
         assert res.status_code == 200
         print(res.status_code)
         print(response)
 
 
 
-
+    # Bug
     def test_search_order_correct_with_etrado_payment(self):
         body = {"search": "etrado"}
         res = requests.post(self.url, json=body)
@@ -118,10 +117,12 @@ class TestSearch:
 
 
 
+
     def test_search_order_correct_with_invalid_payment(self):
         body = {"search": "לאומי"}
         res = requests.post(self.url, json=body)
         response = res.json()
+        assert response['payload']['count'] == 0
         assert res.status_code == 200
         print(res.status_code)
         print(response)
