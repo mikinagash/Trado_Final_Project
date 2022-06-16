1bbefb7a24de11a652912e40a951c9dbf5b1baf7
miki <mikinagash1997@gmail.com>
2022-06-16 11:17:38 +0300

    commit number 11

--- Utils/Utils.py
+++ Utils/Utils.py
@@ -4,15 +4,15 @@ from selenium import webdriver
 
 
 class Utils():
-    def __init__(self,driver,pic):
+    def __init__(self,driver):
         self.driver = driver
 
-    def validtion(self,expected, actual):
+    def validtion(self,expected, actual,pic):
         driver = self.driver
         try:
             assert expected == actual
         except Exception:
-            allure.attach(driver.get_screenshot_as_png(), driver.save_screenshot("pic"),
+            allure.attach(driver.get_screenshot_as_png(), driver.save_screenshot(pic),
                           attachment_type=allure.attachment_type.PNG)
 
 
